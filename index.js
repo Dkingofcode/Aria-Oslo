db966b75a8008057a7b0019178ff40ef5ac4bad4
Dkingofcode <doladepo128@gmail.com>
2022-07-31 16:11:29 +0100

    added animations with javascript

---
+++ index.js
@@ -0,0 +1,62 @@
+/*const sectionOne = document.querySelector(".pro");
+/*const sections = document.querySelectorAll(".project");*/
+
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
+
+const options = { 
+    root: null,
+    threshold: 0,
+    rootMargin: "-150px"
+};
+
+/*const observer = new IntersectionObserver(function(entries, observer){
+   entries.forEach(entry => {
+     if(!entry.isIntersecting){
+        return;
+     }
+    console.log(entry.target);
+    observer.unobserve(entry.target)
+   });
+}, options);
+
+sections.forEach(section => {
+    observer.observe(section);
+})
+
+
+observer.observe(sectionOne);
+*/
+const appearOnScroll = new IntersectionObserver(function(
+    entries, appearOnScroll){
+        entries.forEach(entry => {
+          if(!entry.isIntersecting) {
+            return;
+          }  else {
+            entry.target.classList.add("appear");
+            appearOnScroll.unobserve(entry.target);
+          }
+        });
+    }, options)
+
+  faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+  });
+
+  sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
